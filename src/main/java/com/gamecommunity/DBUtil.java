9d3972a18fe324786965eb2482800fde70b7415a
jusanghyeok <jsmh991102@gmail.com>
2026-08-28 12:20:23 +0900

    공용DB 적용

--- src/main/java/com/gamecommunity/DBUtil.java
+++ src/main/java/com/gamecommunity/DBUtil.java
@@ -6,13 +6,13 @@ import java.sql.DriverManager;
 public class DBUtil {
 
     private static final String URL =
-            "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+            "jdbc:oracle:thin:@(description=(retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.ap-tokyo-1.oraclecloud.com))(connect_data=(service_name=g9587fb0aea9019_gamecommunity_tp.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))";
 
     private static final String USER =
-            "gamecommunity";
+            "GAMECOMMUNITY";
 
     private static final String PASSWORD =
-            "1234";
+            "Classkosa2188";
 
     public static Connection getConnection() throws Exception {
         Class.forName("oracle.jdbc.OracleDriver");
