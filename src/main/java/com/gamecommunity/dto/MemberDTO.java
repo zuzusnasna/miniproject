549a2716444f8ef69b253308755787b7ac7d673a
jusanghyeok <jsmh991102@gmail.com>
2026-08-28 15:23:45 +0900

    feat: 게시글 삭제 권한 처리를 위한 회원 역할 정보 추가

--- src/main/java/com/gamecommunity/dto/MemberDTO.java
+++ src/main/java/com/gamecommunity/dto/MemberDTO.java
@@ -13,6 +13,7 @@ public class MemberDTO {
     private String accountStatus;
     private int receivedLikeCount;
     private int receivedDislikeCount;
+    private String roleName;
 
     public String getAccountStatus() {
         return accountStatus;
@@ -89,6 +90,7 @@ public class MemberDTO {
     public int getReceivedLikeCount() {
         return receivedLikeCount;
     }
+
     public void setReceivedLikeCount(int receivedLikeCount) {
         this.receivedLikeCount = receivedLikeCount;
     }
@@ -96,9 +98,16 @@ public class MemberDTO {
     public int getReceivedDislikeCount() {
         return receivedDislikeCount;
     }
+
     public void setReceivedDislikeCount(int receivedDislikeCount) {
         this.receivedDislikeCount = receivedDislikeCount;
     }
 
+    public String getRoleName() {
+        return roleName;
+    }
 
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
