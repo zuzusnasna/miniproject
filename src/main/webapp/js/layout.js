e50cac0ba170a0937c3414609d5d853aaaead5b1
jusanghyeok <jsmh991102@gmail.com>
2026-09-01 11:08:10 +0900

    fix: 상단바 비로그인 회원정보 상태 초기화

--- src/main/webapp/js/layout.js
+++ src/main/webapp/js/layout.js
@@ -21,8 +21,8 @@ function initLayout() {
                                 <path d="M12 22c1.1 0 2-.9 2-2h-4c0 1.1.9 2 2 2zm6-6v-5c0-3.07-1.63-5.64-4.5-6.32V4c0-.83-.67-1.5-1.5-1.5S10.5 3.17 10.5 4v.68C7.64 5.36 6 7.92 6 11v5l-2 2v1h16v-1l-2-2zm-2 1H8v-6c0-2.48 1.51-4.5 4-4.5s4 2.02 4 4.5v6z"/>
                             </svg>
                         </button>
-                        <button type="button" class="member-menu-trigger" id="memberMenuTrigger" aria-expanded="false" aria-controls="commonMemberInfo">
-                            회원정보 <span class="member-menu-arrow">▼</span>
+                        <button type="button" class="member-menu-trigger" id="memberMenuTrigger" aria-expanded="false" aria-controls="commonMemberInfo" disabled>
+                            Not login status
                         </button>
                     </div>
                 </div>
